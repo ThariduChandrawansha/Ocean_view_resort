ec15192ee85625054e0a6b77fb805625d4e5a97f
Tharindu Chandrawansha <Tharinduchandrawanshajr@gmail.com>
2026-02-13 13:15:43 +0530

    All database models are implemented to the backend

---
+++ backend/src/main/java/com/oceanview/backend/model/Invoice.java
@@ -0,0 +1,20 @@
+package com.oceanview.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "invoices")
+public class Invoice {
+    @Id
+    private String id;
+    
+    private String guestId;
+    private Double subtotal;
+    private String reservationId;
+}
---
+++ backend/src/main/java/com/oceanview/backend/model/Reservation.java
@@ -0,0 +1,30 @@
+package com.oceanview.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "reservations")
+public class Reservation {
+    @Id
+    private String id;
+    
+    private String guestId;
+    private String roomId;
+    
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    
+    private Double totalCost;
+    private Integer totalNights;
+    
+    private ReservationStatus status;
+    private String notes;
+    private PaymentStatus paymentStatus;
+}
